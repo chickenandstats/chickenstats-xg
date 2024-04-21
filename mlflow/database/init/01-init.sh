edafacc75779b43b2cd5ce419abc72d0bbbce5e1
chickenandstats <chicken@chickenandstats.com>
2024-04-20 23:34:07 -0500

    Fixing initial script for postgres db

--- mlflow/database/init/01-init.sh
+++ mlflow/database/init/01-init.sh
@@ -4,12 +4,10 @@ export PGPASSWORD=$POSTGRES_PASSWORD;
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   CREATE USER "$APP_DB_USER" WITH PASSWORD '$APP_DB_PASSWORD';
   CREATE DATABASE "$APP_DB" WITH OWNER "$APP_DB_USER";
-  GRANT ALL PRIVILEGES ON DATABASE mlflow TO "$APP_DB_USER";
-  CREATE DATABASE optuna WITH OWNER "$APP_DB_USER";
-  GRANT ALL PRIVILEGES ON DATABASE optuna TO "$APP_DB_USER";
+  GRANT ALL PRIVILEGES ON DATABASE "$APP_DB" TO "$APP_DB_USER";
 
 EOSQL
 
-psql --username "$APP_DB_USER" mlflow < /backups/mlflow.sql
+#psql --username "$APP_DB_USER" mlflow < /backups/mlflow.sql
 
 #psql --username "$APP_DB_USER" optuna < /backups/optuna.sql
