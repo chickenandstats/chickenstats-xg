4af5f93fb5f250fc618d0b701379cb869ddcd075
chickenandstats <chicken@chickenandstats.com>
2024-05-27 17:12:52 -0500

    Adding API database

---
+++ api/database/init/01-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER "$APP_DB_USER" WITH PASSWORD '$APP_DB_PASSWORD';
+  CREATE DATABASE evolving_hockey WITH OWNER "$APP_DB_USER";
+  GRANT ALL PRIVILEGES ON DATABASE evolving_hockey TO "$APP_DB_USER";
+  CREATE DATABASE chicken_nhl WITH OWNER "$APP_DB_USER";
+  GRANT ALL PRIVILEGES ON DATABASE chicken_nhl TO "$APP_DB_USER";
+
+EOSQL
+
+#psql --username "$APP_DB_USER" mlflow < /backups/mlflow.sql
+
+#psql --username "$APP_DB_USER" optuna < /backups/optuna.sql
